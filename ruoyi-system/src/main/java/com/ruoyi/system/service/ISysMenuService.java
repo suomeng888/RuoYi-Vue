48d64bb55d87d6181bcc7fd2b64b4102b138be56
meng.suo <meng.suo@51job.com>
2023-10-11 15:18:40 +0800

    12

--- ruoyi-system/src/main/java/com/ruoyi/system/service/ISysMenuService.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/service/ISysMenuService.java
@@ -19,7 +19,7 @@ public interface ISysMenuService
      * @param userId 用户ID
      * @return 菜单列表
      */
-    public List<SysMenu> selectMenuList1(Long userId);
+    public List<SysMenu> selectMenuList(Long userId);
 
     /**
      * 根据用户查询系统菜单列表
